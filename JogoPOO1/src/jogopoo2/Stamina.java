f79466f9f7a5ca191b04cec02687fc40ef98c850
LucasManco <lucasmancosouza@hotmail.com>
2018-03-19 11:31:27 -0300

    Add files via upload

---
+++ JogoPOO1/src/jogopoo2/Stamina.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogopoo2;
+
+/**
+ *
+ * @author 20132bsi0133
+ */
+public class Stamina {
+    
+}
